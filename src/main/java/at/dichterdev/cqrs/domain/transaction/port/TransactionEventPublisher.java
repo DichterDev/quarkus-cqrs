7d524b65d63774df795e039b1694c1cd915a762c
DichterDev <dichterdev.null@gmail.com>
2026-02-20 12:13:48 +0100

    fix: Transaction Event Publisher

--- src/main/java/at/dichterdev/cqrs/domain/transaction/port/TransactionEventPublisher.java
+++ src/main/java/at/dichterdev/cqrs/domain/transaction/port/TransactionEventPublisher.java
@@ -1,8 +1,7 @@
 package at.dichterdev.cqrs.domain.transaction.port;
 
 import at.dichterdev.cqrs.domain.common.DomainEventPublisher;
-import at.dichterdev.cqrs.domain.transaction.event.TransactionEvent;
 
-public interface TransactionEventPublisher extends DomainEventPublisher<TransactionEvent> {
+public interface TransactionEventPublisher extends DomainEventPublisher {
 
 }
